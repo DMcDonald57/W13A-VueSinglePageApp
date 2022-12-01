f6dd9c6713c78ff5c854d9a4dd4a2601e17580f7
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-30 20:38:18 -0700

    Added some images to footer and some styling

--- src/components/pageBody.vue
+++ src/components/pageBody.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
+        <p>Slowly getting the hang of this but I would have thought
+            that this body portion would be under the header.
+        </p>
+        <p>Maybe this will be something we could get clarity on ......wait a minute
+            the page is moving down the more I type.
+        </p>
+        <p>Interesting.  If I am understanding this properly if I where to use only the 
+            header, it would not include this portion of the page or the bottom images
+            but since I have imported all three onto this page, that is why they are all 
+            displayed.
+        </p>
 
     </div>
 </template>
--- src/components/pageFooter.vue
+++ src/components/pageFooter.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-
+        <h3>Lets add more images in the footer portion!</h3>
+        <img src="https://i.pinimg.com/280x280_RS/e2/8c/33/e28c33b0b1ed3ebfb0a42b1f844432b8.jpg" alt="Nose">
+        <img src="https://i.stack.imgur.com/aqN2A.png?s=256&g=1" alt="flower">
     </div>
 </template>
 
@@ -11,5 +13,12 @@
 </script>
 
 <style scoped>
+
+h3 {
+    font-family: cursive;
+    color: red;
+    text-shadow:black 1px 1px 8px;
+}
+
 
 </style>
